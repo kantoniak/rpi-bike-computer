8cd0abf63fdd4d42634fafd9fa885ae1faa16cce
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-09-04 21:17:05 +0200

    Add display handler

--- counter.py
+++ counter.py
@@ -1,6 +1,10 @@
+import Adafruit_SSD1306
 import RPi.GPIO as GPIO
 import time
 import signal
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 should_close = False
 
@@ -8,18 +12,26 @@ def handle_signals(signum, frame):
     global should_close
     should_close = True
 
+signal.signal(signal.SIGINT, handle_signals)
+signal.signal(signal.SIGTERM, handle_signals)
+
+# Diode pins
 LED_PIN = 17
 BTN_PIN = 27
 LAP_PIN = 4
-
-signal.signal(signal.SIGINT, handle_signals)
-signal.signal(signal.SIGTERM, handle_signals)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(BTN_PIN, GPIO.IN)
 GPIO.setup(LAP_PIN, GPIO.OUT, initial=GPIO.LOW)
+
+# Display
+display = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+display.begin()
+display.clear()
+display.display()
+image = Image.new('1', (display.width, display.height))
 
 was_closed = GPIO.input(BTN_PIN)
 first_lap = True
@@ -31,10 +43,13 @@ while not should_close:
 
     if (was_closed and not closed):
         new_time = time.time()
-        print('LAP: ' + str(new_time - last_lap))
+        print('LAP: ' + str(new_time - last_lap)
         last_lap = new_time
         GPIO.output(LAP_PIN, GPIO.HIGH)
         lap_on = True
+
+        disp.image(image)
+        disp.display()
 
     if lap_on:
         delta = time.time() - last_lap
